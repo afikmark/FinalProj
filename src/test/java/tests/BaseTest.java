31ef4ef8236bd7123862d6a0dc74aa7c92bd3a6f
Afik Mark <78296112+afikmark@users.noreply.github.com>
2021-08-04 16:31:18 +0300

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,30 +1,42 @@
 package tests;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.internal.Utils;
 import utils.Utils1;
 
-
 public class BaseTest {
-
+    Utils1 u = new Utils1();
     WebDriver driver;
+
 
     @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "C:\\WebSelenium\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        Utils1 u = new Utils1();
         driver.get(u.readProperty("url"));
-
+        String title = driver.getTitle();
+        if (u.readProperty("PageTitle").equals(title)) {
+            System.out.println("Title is correct " + title);
+        } else {
+            int attempts = 0;
+            while (attempts < 3) {
+                System.out.println("Error found, Restarting...");
+                driver.quit();
+                setup();
+                attempts++;
+            }
+        }
     }
-    public String getURL(){
-        return driver.getCurrentUrl();
 
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
+
+
 }
+
