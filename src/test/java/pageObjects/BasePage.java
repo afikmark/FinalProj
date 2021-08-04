ef120806b0a8fabbe550393baa47e4476cc4280a
Afik Mark <78296112+afikmark@users.noreply.github.com>
2021-08-04 16:30:26 +0300

    Update BasePage.java

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -1,20 +1,28 @@
 package pageObjects;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import utils.Utils1;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 
 public class BasePage {
     WebDriver driver;
+    Utils1 u = new Utils1();
+
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+
+    }
+
+    public BasePage() {
 
     }
 
@@ -32,13 +40,19 @@ public class BasePage {
     }
 
     public void explicitWaitVisibility(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public void explicitWaitClickable(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+
+    public void explicitWaitAllElements(List<WebElement> elements) {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElements(elements));
     }
 
     public void SelectByValue(WebElement el, String Value) {
